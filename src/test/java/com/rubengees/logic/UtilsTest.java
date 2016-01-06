3ce7e8a4b72c8621f0d8e55ac7648cbba0abd6ad
Ruben Gees <geesruben@yahoo.de>
2016-01-06 02:10:54 +0100

    Add a test for the cloneMatrix mehod

---
+++ src/test/java/com/rubengees/logic/UtilsTest.java
@@ -0,0 +1,32 @@
+package com.rubengees.logic;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+/**
+ * TODO: Describe Class
+ *
+ * @author Ruben Gees
+ */
+public class UtilsTest {
+
+    Cell[][] matrix;
+
+    @Before
+    public void setUp() throws Exception {
+        matrix = new Cell[2][2];
+
+        matrix[0][0] = new Cell(true, 0, 0);
+        matrix[0][1] = new Cell(true, 0, 1);
+        matrix[1][0] = new Cell(false, 1, 0);
+        matrix[1][1] = new Cell(true, 1, 1);
+    }
+
+    @Test
+    public void testCloneMatrix() throws Exception {
+        Assert.assertTrue(Arrays.deepEquals(matrix, Utils.cloneMatrix(matrix)));
+    }
+}
